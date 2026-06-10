00e3761453f2a0975a49bf7de97e8bc881d99d8d
Tsiko01 <bacho.wiklauri2009@gmail.com>
2026-06-11 00:09:03 +0400

    add title

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -5,13 +5,18 @@ export default function Project() {
   const projects = [
   { name:"about me", github: "https://github.com/Tsiko01/homework-1", vercel: "https://homework-1-alpha-inky.vercel.app/", image: "/favicon.svg" },
   { name:"Batman Shop", github: "https://github.com/Tsiko01/homework-2", vercel: "https://homework-2-beryl.vercel.app/", image: "/Batman PNG.png" },
-  { name:"UFC Fighters", github: "https://github.com/Tsiko01/homework-3", vercel: "https://homework-3-weld-delta.vercel.app/", image: "/favicon.svg" },
-  { name:"TO Do app", github: "https://github.com/Tsiko01/homework-4", vercel: "https://homework-4-five.vercel.app/", image: "/favicon.svg" },
-  { name:"Deteqtor", github: "https://github.com/Tsiko01/homework-5", vercel: "https://homework-5-sooty.vercel.app/", image: "/favicon.svg" },
-  { name:"Accept advice", github: "https://github.com/Tsiko01/homework-6", vercel: "https://homework-6-dusky.vercel.app/", image: "/favicon.svg" },
-  { name:"Raiting app", github: "https://github.com/Tsiko01/homework-7", vercel: "https://homework-7-taupe.vercel.app/", image: "/favicon.svg" },
+  { name:"UFC Fighters", github: "https://github.com/Tsiko01/homework-3", vercel: "https://homework-3-weld-delta.vercel.app/", image: "/ilia.jpeg" },
+  { name:"TO Do app", github: "https://github.com/Tsiko01/homework-4", vercel: "https://homework-4-five.vercel.app/", image: "/do.svg" },
+  { name:"Deteqtor", github: "https://github.com/Tsiko01/homework-5", vercel: "https://homework-5-sooty.vercel.app/", image: "/search.svg" },
+  { name:"Accept advice", github: "https://github.com/Tsiko01/homework-6", vercel: "https://homework-6-dusky.vercel.app/", image: "/ok.svg" },
+  { name:"Raiting app", github: "https://github.com/Tsiko01/homework-7", vercel: "https://homework-7-taupe.vercel.app/", image: "/star.svg" },
   { name:"EXAM 20/20", github: "https://github.com/Tsiko01/homework-8", vercel: "https://homework-8-dusky.vercel.app/", image: "/img.png" },
-  { name:"Z00M, Timer, Video  ", github: "https://github.com/Tsiko01/homework-9", vercel: "https://homework-9-xi.vercel.app/", image: "/favicon.svg" }
+  { name:"Z00M, Timer, Video  ", github: "https://github.com/Tsiko01/homework-9", vercel: "https://homework-9-xi.vercel.app/", image: "/3d.svg" },
+  { name:"Planets ", github: "https://github.com/Tsiko01/homework-11", vercel: "homework-11-nu.vercel.app", image: "/planet-earth-internal.svg" },
+  { name:"Blog araa saintereso  ", github: "https://github.com/Tsiko01/homework-12", vercel: "homework-12-mocha.vercel.app", image: "/bad.svg" },
+  { name:"fantelebi  ", github: "https://github.com/Tsiko01/homework-13", vercel: "homework-13-rust.vercel.app", image: "/favicon.svg" },
+  { name:"Exam inglisuri sityvebi  ", github: "https://github.com/Tsiko01/homework-14", vercel: "homework-14-zeta.vercel.app", image: "/gg.svg" }
+
 ];
 
   return (
